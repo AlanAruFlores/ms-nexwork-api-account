36961e6143d9e461b0d185dbe4515aa48c1cbbcc
AlanAruFlores <alanaruquipa242@gmail.com>
2025-04-28 22:19:15 -0300

    first commit

---
+++ src/main/java/com/nexwork/api/account/ms_nexwork_api_account/MsNexworkApiAccountApplication.java
@@ -0,0 +1,13 @@
+package com.nexwork.api.account.ms_nexwork_api_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNexworkApiAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNexworkApiAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexwork/api/account/ms_nexwork_api_account/MsNexworkApiAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexwork.api.account.ms_nexwork_api_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNexworkApiAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
