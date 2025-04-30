a47d043505752acc9ed6cc4357c1ca7365c746a1
AlanAruFlores <alanaruquipa242@gmail.com>
2025-04-29 23:20:42 -0300

    minor: utility class

---
+++ src/main/java/com/nexwork/api/account/ms_nexwork_api_account/utils/Converter.java
@@ -0,0 +1,15 @@
+package com.nexwork.api.account.ms_nexwork_api_account.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Converter {
+    
+    public static <T> T convertToEntity(Object object, Class<T> entityClass) {
+        return null;
+    }
+
+    public static <T> T convertToDto(Object object, Class<T> dtoClass) {
+        return null;
+    }
+}
