97338726d5bca4fa6d7ea08f587724cde7664833
AlanAruFlores <alanaruquipa242@gmail.com>
2025-05-09 00:21:04 -0300

    feat: creating entities implemented

--- src/main/java/com/nexwork/api/account/ms_nexwork_api_account/dto/ErrorResponseDto.java
+++ src/main/java/com/nexwork/api/account/ms_nexwork_api_account/dto/ErrorResponseDto.java
@@ -3,7 +3,6 @@ package com.nexwork.api.account.ms_nexwork_api_account.dto;
 import lombok.Data;
 
 @Data
-@Builder
 public class ErrorResponseDto {
     private String code;
     private String message;
